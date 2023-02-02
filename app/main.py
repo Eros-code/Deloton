6954f02deb52cf94606f1d2b7fb87767753504a8
eLilyPad <elizabethbryce00@gmail.com>
2023-02-02 14:39:28 +0000

    Add Email Notifaction

--- app/main.py
+++ app/main.py
@@ -1,65 +1,30 @@
-import json
-from confluent_kafka import Consumer
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-server = os.getenv("KAFKA_SERVER")
-username = os.getenv("KAFKA_KEY")
-password = os.getenv("KAFKA_SECRET")
+import boto3
+from botocore.exceptions import ClientError, WaiterError
 
 
-class Kafka:
-    def __init__(self, group_id: str = 'triple-e-consumer'):
-        self.kafka_topic_name = 'deloton'
-        self.consumer: Consumer = Consumer({
-            'bootstrap.servers': server,
-            'group.id': group_id,
-            'security.protocol': 'SASL_SSL',
-            'sasl.mechanisms': 'PLAIN',
-            'sasl.username': username,
-            'sasl.password': password,
-            'session.timeout.ms': 6000,
-            'heartbeat.interval.ms': 1000,
-            'fetch.wait.max.ms': 6000,
-            'auto.offset.reset': 'earliest',
-            'enable.auto.commit': 'false',
-            'max.poll.interval.ms': '86400000',
-            'topic.metadata.refresh.interval.ms': "-1",
-            "client.id": 'id-002-005',
-        })
-            
+class HRTNotifcation:
+    def __init__(self):
+        self.client = boto3.client('sns')
+        self.topic_arn = 'arn:aws:sns:eu-west-2:605126261673:HRT-Email-Notification'
 
-    def _print_msg(self, msg):
-        print(
-        f"""
-            topic:{msg.topic()}, key:{msg.key()}, 
-            value:{msg.value()}
-        """
-    )
-    
-    def _list_topics(self):
-        return self.consumer.list_topics()
+    def subscribe_sns(self, email_address:str):
+        response = self.client.subscribe(
+            TopicArn = self.topic_arn,
+            Protocol = 'email',
+            Endpoint = email_address,
+            ReturnSubscriptionArn = True,
+        )
+        return response
 
-    def get_data(self, consumer = None, topic_name = None, amount:int = 100_000):
-        if consumer is None:
-            consumer = self.consumer
-        if topic_name is None:
-            topic_name = self.kafka_topic_name
-            
-        consumer.subscribe([topic_name])
-        
-        values = []
-        for i in range(amount):
-            msg = consumer.poll(1.0)
-            if msg:
-                self._print_msg(msg)
-                values.append(json.loads(msg.value().decode('utf-8')))
-            
-        return values
+    def send_notification(self,msg:str):
+        self.client.publish(
+            TopicArn=self.topic_arn,
+            Message = msg
+            )
 
+def main():
+    emailer = HRTNotifcation()
+    emailer.send_notification('this is a test notification')
 
-
-k = Kafka()
-# print(k._list_topics())
-data = k.get_data(amount=100)
+if __name__ == '__main__':
+    main()
